4eabab13cba5b3c9865852dd70a67c14340f3662
alioh <lo.ol.123@gmail.com>
2015-03-25 10:00:43 -0400

    fix some html/css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+  	if signed_in?
+  		redirect_to properties_path
+  	end
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
 		direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
 		link_to title, :sort => column, :direction => direction
 	end
+
+	def username
+		n = User.find(current_user)
+		n.name
+	end
+
 end
