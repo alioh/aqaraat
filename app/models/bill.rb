1ce8dae28a8163cd55b5b60f7ae963d92a6021d8
alioh <lo.ol.123@gmail.com>
2015-03-28 12:41:26 -0400

    fix validation error

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,6 +2,7 @@ class Bill < ActiveRecord::Base
 	belongs_to :property
 	belongs_to :user
 	belongs_to :redisent
+	validates_presence_of :resident_id, :addresstype, :dudeate, :amount, :billtype, :status
 
 	def self.total_on(date)
 		where("date(duedate) = ?", date).sum(:amount)
